b02f428aff4ed143ab6728ff99816b7d3fc84933
monjurulhaquerajun <monjurulhaquerajun@gmail.com>
2025-03-06 16:50:40 +0600

    using-random

--- blackJack-game/script.js
+++ blackJack-game/script.js
@@ -1,20 +1,38 @@
-let firstCard = 10
-let secondCard = 9
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 gotBlackJack = false
 isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
+function getRandomCard(){
+    return Math.floor(Math.random()*13)
+}
 function startGame(){
+    renderGame()
+}
+
+function renderGame(){
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     sumEl.textContent = "Sum: "+ sum
     if(sum <= 20){
         message = "Do you want to draw a new card?"
-    }else if(sum === 20){
+    }else if(sum === 21){
         message = "You have got BlackJack"
+        gotBlackJack = true
     }else{
         message = "You are out of the game"
         isAlive = false
     }
+    messageEl.textContent = message
+}
+
+function newCard(){
+    let card = 2
+    sum += card
+    renderGame()
 }
